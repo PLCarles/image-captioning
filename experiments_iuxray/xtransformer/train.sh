80e227f08cbb979d134a191c2238526f700fd364
Pan Li <53412346+PLCarles@users.noreply.github.com>
2021-06-13 19:01:52 +1000

    Update train.sh

--- experiments_iuxray/xtransformer/train.sh
+++ experiments_iuxray/xtransformer/train.sh
@@ -1,3 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 main.py --folder ./experiments_iuxray/xtransformer  --resume 0 --training_ratio 1
+CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 main.py --folder ./experiments_iuxray/xtransformer  \
+                                --resume 0 --training_ratio 1 --feed_mode GCNCNN
 
 ###if you want to use checkpoint, download your model in experiments_mimiccxr/xtransformer/snapshot and change 0 to your model's number
